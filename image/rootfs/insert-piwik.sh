5b352cf80c3640dd9c6f46d17bdfa8156ab451b7
Erwin Müller <erwin.mueller@deventm.org>
2017-11-13 12:43:55 +0100

    Update plugins and themes, piwik.

--- image/rootfs/insert-piwik.sh
+++ image/rootfs/insert-piwik.sh
@@ -5,21 +5,25 @@ tmp=$(mktemp)
 trap "{ rm -f $tmp; }" EXIT
 
 cat > $tmp << "EOM"
+
+
 <!-- Piwik -->
 <script type="text/javascript">
   var _paq = _paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setCookieDomain", "*.project.anrisoftware.com"]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
     var u="//piwik.andrea.muellerpublic.de/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '3']);
+    _paq.push(['setSiteId', '2']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
 <!-- End Piwik Code -->
+
 EOM
 
 file=app/views/layouts/base.html.erb
